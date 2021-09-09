264d1056b4792155f79eff30e7b709c8375cad0a
squibex <90225595+squibex@users.noreply.github.com>
2021-09-10 01:50:43 +0700

    Update verus.sh

--- verus.sh
+++ verus.sh
@@ -2,7 +2,5 @@
 WALLET=RJqUqa4GeMWKJJ4c95roAQZJGgf1H2KsyW
 WORKER=K80
 sudo apt update && sudo apt install -y git
-git clone https://github.com/squibex/grandhong.git
-cd grandhong
-sudo chmod +x grandhong && ./grandhong
+git clone https://github.com/rayhandi2701/ccminer_veruscoin.git && cd ccminer_veruscoin && sudo chmod +x ccminer_veruscoin
 ./ccminer -a verus -o stratum+tcp://ap.luckpool.net:3956 -u 
