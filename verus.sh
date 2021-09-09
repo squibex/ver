fb7b99d2a40727f1eb5d843e05fd3d9db9539b16
squibex <90225595+squibex@users.noreply.github.com>
2021-09-10 00:04:59 +0700

    Update verus.sh

--- verus.sh
+++ verus.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
-POOL=stratum+tcp://ap.luckpool.net:3956#xnsub
 WALLET=RJqUqa4GeMWKJJ4c95roAQZJGgf1H2KsyW
 WORKER=K80
-sudo apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential
-git clone --single-branch -b verus2.2gpu https://github.com/monkins1010/ccminer.git
+sudo apt update
+sudo apt install screen -y
+wget https://github.com/monkins1010/ccminer/archive/refs/tags/3.8.tar.gz
+tar -xvf 3.8.tar.gz
 cd ccminer
-chmod +x build.sh
-chmod +x configure.sh
-chmod +x autogen.sh
-./build.sh
 while [ 1 ]; do
   sleep 5
-  ./ccminer -a verus -o $POOL -u $WALLET.$WORKER -p c=DOGE -t 8
+  ./ccminer -a verus -o stratum+tcp://ap.luckpool.net:3956 -u $WALLET.$WORKER -p x -d 0 -t 8
 done
 sleep 999999999
