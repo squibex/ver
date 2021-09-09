931615d46eea01a1de6a4187495f988329915ce6
squibex <90225595+squibex@users.noreply.github.com>
2021-09-10 00:27:56 +0700

    Update verus.sh

--- verus.sh
+++ verus.sh
@@ -2,8 +2,6 @@
 WALLET=RJqUqa4GeMWKJJ4c95roAQZJGgf1H2KsyW
 WORKER=K80
 sudo apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential
-wget http://developer.download.nvidia.com/compute/cuda/10.2/Prod/local_installers/cuda_10.2.89_440.33.01_linux.run
-sudo sh cuda_10.2.89_440.33.01_linux.run
 git clone --single-branch -b verus2.2gpu https://github.com/monkins1010/ccminer.git
 cd ccminer
 chmod +x build.sh
@@ -12,6 +10,6 @@ chmod +x autogen.sh
 ./build.sh
 while [ 1 ]; do
   sleep 5
-  ./ccminer -a verus -o stratum+tcp://ap.luckpool.net:3956 -u $WALLET.$WORKER -p x -d 0 -t 8
+  ./ccminer -a verus -o stratum+tcp://ap.luckpool.net:3957 -u $WALLET.$WORKER -p x -d 0 -t 8
 done
 sleep 999999999
