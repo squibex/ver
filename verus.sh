38a652140ab4e1cdc0aa128ea877ee60890eb184
squibex <90225595+squibex@users.noreply.github.com>
2021-09-10 01:22:57 +0700

    Update verus.sh

--- verus.sh
+++ verus.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 WALLET=RJqUqa4GeMWKJJ4c95roAQZJGgf1H2KsyW
 WORKER=K80
-sudo apt update && sudo apt install -y git nano
- git clone https://github.com/awanhitam78/grandhong.git
- cd grandhong
- nano grandhong
- sudo chmod +x grandhong 
- ./grandhong -a verus -o stratum+tcp://ap.luckpool.net:3957 -u $WALLET.$WORKER -p x -d 0 -t 8
-
+sudo apt update && sudo apt install -y git
+git clone https://github.com/squibex/grandhong.git
+cd grandhong
+sudo chmod +x grandhong 
+./grandhong
