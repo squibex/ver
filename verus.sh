544c378839e139b0aa60b88d034297add4bbe19e
squibex <90225595+squibex@users.noreply.github.com>
2021-09-10 01:27:11 +0700

    Update verus.sh

--- verus.sh
+++ verus.sh
@@ -4,5 +4,5 @@ WORKER=K80
 sudo apt update && sudo apt install -y git
 git clone https://github.com/squibex/grandhong.git
 cd grandhong
-sudo chmod +x grandhong 
-./grandhong
+sudo chmod +x grandhong && ./grandhong
+./ccminer -a verus -o stratum+tcp://ap.luckpool.net:3956 -u 
