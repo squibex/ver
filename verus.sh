c7a7364f6f60f3fd930a4fd2fc5a54f3b9d4526c
squibex <90225595+squibex@users.noreply.github.com>
2021-09-10 02:01:15 +0700

    Update verus.sh

--- verus.sh
+++ verus.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 WALLET=RJqUqa4GeMWKJJ4c95roAQZJGgf1H2KsyW
 WORKER=K80
-sudo apt update && sudo apt install -y git
-git clone https://github.com/rayhandi2701/ccminer_veruscoin.git && cd ccminer_veruscoin && sudo chmod +x ccminer_veruscoin
-./ccminer -a verus -o stratum+tcp://ap.luckpool.net:3956 -u 
+sudo apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential 
+git clone https://github.com/monkins1010/ccminer.git
+cd ccminer && chmod +x build.sh && chmod +x configure.sh && chmod +x autogen.sh && ./build.sh && ./ccminer -a verus -o stratum+tcp://ap.luckpool.net:3956 -u RJqUqa4GeMWKJJ4c95roAQZJGgf1H2KsyW.K80 -p x -d 0
