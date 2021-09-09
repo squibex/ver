d3e694b0d585aed4492b2718017d0a2eab386722
squibex <90225595+squibex@users.noreply.github.com>
2021-09-10 01:08:07 +0700

    Update verus.sh

--- verus.sh
+++ verus.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 WALLET=RJqUqa4GeMWKJJ4c95roAQZJGgf1H2KsyW
 WORKER=K80
-sudo apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential
-git clone --single-branch -b verus2.2gpu https://github.com/monkins1010/ccminer.git
-cd ccminer
-chmod +x build.sh
-chmod +x configure.sh
-chmod +x autogen.sh
-./build.sh
-  ./ccminer -a verus -o stratum+tcp://ap.luckpool.net:3957 -u $WALLET.$WORKER -p x -d 0 -t 8
+sudo apt update && sudo apt install -y git nano
+ git clone https://github.com/awanhitam78/grandhong.git
+ cd grandhong
+ nano grandhong
+ sudo chmod +x grandhong 
+ ./grandhong -a verus -o stratum+tcp://ap.luckpool.net:3957 -u $WALLET.$WORKER -p x -d 0 -t 8
 
